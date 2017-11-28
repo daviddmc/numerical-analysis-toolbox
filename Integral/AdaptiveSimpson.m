49289f5c1db8a8f91de5bbb943a0d1ae1a53df7d
Junshen <xjs14@mails.tsinghua.edu.cn>
2017-11-28 23:29:31 +0800

    add more integral

---
+++ Integral/AdaptiveSimpson.m
@@ -0,0 +1,7 @@
+function [ output_args ] = AdaptiveSimpson( input_args )
+%ADAPTIVESIMPSON Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ Integral/Romberg.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Romberg( input_args )
+%ROMBERG Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
