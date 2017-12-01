c0eeda9ace88764dd04d04245a8766d975155466
Junshen <xjs14@mails.tsinghua.edu.cn>
2017-12-01 17:59:34 +0800

    update FFT

--- approximation/FFT.m
+++ approximation/FFT.m
@@ -2,37 +2,57 @@ function x = FFT( x )
 %FFT Summary of this function goes here
 %   Detailed explanation goes here
 
-x = Base3FFT(x);
+N = length(x);
+
+if N <= 128
+    n = 0 : N-1;
+    k = n';
+    M = exp(-2j * pi * bsxfun(@times, n, k) / N);
+    x = reshape(x, [1, N]) * M;
+else
+    [N1, N2, flag] = FactorHelper(N);
+        
+    x = reshape(x, [N1, N2]);
+    
+    if(N2 > 1)
+        for ii = 1 : N1
+            x(ii, :) = FFT(x(ii, :));
+        end
+        fac = exp(-2j * pi / N * bsxfun(@times, (0: N1-1)',0 : N2-1));
+        x = fac .* x;
+    end
+    
+    switch(flag)
+        case 0
+            for jj = 1 : N2
+                x(:, jj) = FFT(x(:, jj));
+            end
+        case 1
+            for jj = 1 : N2
+                x(:, jj) = Base2FFT(x(:, jj));
+            end
+        case 2
+            for jj = 1 : N2
+                x(:, jj) = Base3FFT(x(:, jj));
+            end
+    end
+
+    x = x.';
+    x = x(:);
+end
 
 end
 
 function x = Base2FFT(x)
 
-
-flag = 0;
-if(iscolumn(x))
-    flag = 1;
-    x = x.';
-end
-
 N = length(x);
-if mod(N, 2) > 0 
-    error(' ');
-end
 
 N_min = min(N, 128);
 n = 0 : N_min-1;
 k = n';
 M = exp(-2j * pi * bsxfun(@times, n, k) / N_min);
 x = reshape(x, [N/N_min, N_min]) * M;
-%{
-while(size(x,1) < N)
-    x_even = x(:, 1:size(x, 2) /2);
-    x_odd = x(:, size(x,2)/2+1:end);
-    factor = exp(-1j * pi * (0:size(x,1)-1)' / size(x,1));
-    x = [x_even + factor .* x_odd; x_even - factor .* x_odd];    
-end
-%}
+
 while(size(x,2) < N)
     x_even = x(1:size(x, 1) /2, :);
     x_odd = x(size(x,1)/2+1:end, :);
@@ -40,22 +60,11 @@ while(size(x,2) < N)
     x = [x_even + factor .* x_odd, x_even - factor .* x_odd];    
 end
 
-if(flag)
-    x = x.';
-end
-
 end
 
 function x = Base3FFT(x)
 
-if(iscolumn(x))
-    x = x';
-end
-
 N = length(x);
-if mod(N, 3) > 0 
-    error(' ');
-end
 
 N_min = min(N, 81);
 n = 0 : N_min-1;
@@ -74,8 +83,39 @@ while(size(x,2) < N)
          x1 + exp(-4j/3*pi) * factor1 .* x2 + exp(-2j/3*pi) * factor2 .* x3];    
 end
 
-if(flag)
-    x = x.';
 end
 
+function [N1, N2, flag] = FactorHelper(N)
+flag = 0;
+f = factor(N);
+e2 = find(f == 2);
+if(~isempty(e2))
+    e2 = e2(end) - e2(1) + 1;
+    N1 = 2^e2;
+    if (N1 >= 8)
+        flag = 1;
+    end   
+end
+
+if(~flag)
+    e3 = find(f == 3);
+    if(~isempty(e3))
+        e3 = e3(end) - e3(1) + 1;
+        N1 = 3^e3;
+        if(N1 >= 9)
+            flag = 2;
+        end
+    end
+end
+
+if(~flag)
+    N1 = 1;
+    for ii = 1:length(f)
+        N1 = N1 * f(ii);
+        if(N1^2 > N)
+            break;
+        end
+    end
+end
+N2 = N / N1;
 end
