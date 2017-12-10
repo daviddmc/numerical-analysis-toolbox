3e4cb4c7ab601aa5d3f98cb7910708bdbd8cc6d4
Junshen <xjs14@mails.tsinghua.edu.cn>
2017-12-10 12:35:19 +0800

    update integral

--- Integral/Simpson.m
+++ Integral/Simpson.m
@@ -1,10 +1,16 @@
 function I = Simpson( f, a, b, n )
-%TRAPEZOID Summary of this function goes here
-%   Detailed explanation goes here
+% Simpson     Numerical integration using Simpson rule.
+%   I = Simpson(FUN, A, B, N) approximates the integral of function FUN
+%   from A to B using Simpson rule. The interval [A, B] will be divided
+%   into N equispaced subintervals.
+%
+%   See also
+
+%   Copyright 2017 Junshen Xu
 
 h = (b - a) / n;
 H = h * sum(f(a-h/2 + h * (1:n)));
-T = Trapezoid(f, a, b, n);
+T = Simpson(f, a, b, n);
 
 I = T/3 + 2*H/3;
 end
--- Integral/Trapezoid.m
+++ Integral/Trapezoid.m
@@ -1,11 +1,15 @@
-function I = Trapezoid( f, a, b, n )
-%TRAPEZOID Summary of this function goes here
-%   Detailed explanation goes here
+function I = Trapezoid( fun, a, b, n )
+% Trapezoid     Numerical integration using trapezoid rule.
+%   I = Trapezoid(FUN, A, B, N) approximates the integral of function FUN
+%   from A to B using trapezod rule. The interval [A, B] will be divided
+%   into N equispaced subintervals.
+%
+%   See also
+
+%   Copyright 2017 Junshen Xu
 
 h = (b - a) / n;
-
-I = h * (f(a) + f(b) + 2 * sum(f(a + h * (1:n-1)))) / 2;
-
+I = h * (fun(a) + fun(b) + 2 * sum(fun(a + h * (1:n-1)))) / 2;
 
 end
 
